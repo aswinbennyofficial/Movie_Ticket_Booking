f10928a460937e95599f792f3eb48f94135acf01
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2023-04-27 19:37:08 +0530

    Update main.c
    fixed login issue

--- main.c
+++ main.c
@@ -1,17 +1,40 @@
 #include <stdio.h>
 
+void adminDash(){
+    printf("welcome admin");
+}
 
+void createAccount(){
+    FILE *file ;
+    char id[20];
+    char pass[20];
 
+    file = fopen("userdata.txt","a");
 
-// void tktHistory(char user[]){
-//     printf("history of %s",user);
-// }
+    if (file == NULL)
+        printf("\nUnable to open the file\n");
+    else{
+        printf("\nEnter UserID: \n");
+        scanf(" %s",&id);
 
-// void bookTkt(char user[]){
-//     printf("Ticket booked for %s",user);
+        printf("Now enter Password: \n");
+        scanf(" %s",&pass);
+
+        strcat(id,pass);
+        printf("Account Created successfully \n\n\n ",id);
+        fprintf(file,id );
+        fprintf(file, "\n");
+        fclose(file);
+        printf("Please Login with the new ID\n");
+        login();
+    }
+
+}
+
 
 void userDash(char user[]){
     int ch;
+    printf("\nWelome %s\n",user);
     printf("Choose one option:\n\n1.Book ticket\n2.See Booking History");
     scanf("%d",&ch);
     switch(ch){
@@ -32,13 +55,8 @@ void userDash(char user[]){
 }
 
 void login(){
-    FILE* ptr = fopen("userdata.txt", "r");
-    if (ptr == NULL) {
-        printf("no such file.");
-        return 0;
-    }
 
-
+    char str[100];
     char chk_username[100]; //takes input username
     char chk_pass[100]; //takes input password
     char authstr[100]; //makes a new array to store concatenated pass and username
@@ -50,23 +68,42 @@ void login(){
     strcpy( authstr,chk_username); // copies chk_username to authstr
     strcat( authstr, chk_pass ); // concatenate chk_pass to authstr
 
-    // validator
-    char buf[100];
-    while (fscanf(ptr, "%s %*s",
-                  buf)
-           == 1){
-         if(strcmp(authstr,"adminpass")==0){
-            fclose(ptr);
-            printf("admin succeess");
-            break;
-        }
-        else if(strcmp(buf,authstr)==0){
-            fclose(ptr);
+    FILE *fp = fopen("userdata.txt", "r");
+    if (fp == NULL) {
+        printf("Error: could not open file.\n");
+        return 1;
+    }
+
+    if(strcmp(authstr,"adminpass")==0){
+
+        printf("admin auth succeess\n");
+        adminDash();
+        fclose(fp);
+        return;
+
+
+    }
+
+    while (fgets(str, 100, fp) != NULL) {
+        str[strcspn(str, "\n")] = '\0'; // remove newline character from input
+        if (strcmp(str, authstr) == 0) {
+            fclose(fp);
             printf("user auth succeess");
             userDash(chk_username);
             break;
         }
     }
+    fclose(fp);
+    int ch;
+    printf("user not found\nDo you want to create a new account?\n1.Create a new account\n2.Login\n");
+    scanf("%d",&ch);
+    if(ch==1){
+        createAccount();
+    }
+    else{
+        login();
+    }
+    return 0;
 
 
 }
@@ -77,4 +114,3 @@ void main(){
     login();
 
 }
-
